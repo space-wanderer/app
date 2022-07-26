5ccb64b1dd8ec2eb64de7f322cf1a657221dc5ae
Vjacheslav Puzanov <v.m.puzanov@yandex.ru>
2022-07-26 23:12:40 +0300

    added game class

--- game.py
+++ game.py
@@ -67,7 +67,53 @@ class Player:
         self.card = None
         self.name = name
 
-deck = Deck()
+class Game:
+    def __init__(self):
+        name1 = input("Имя игрога 1: ")
+        name2 = input("Имя игрога 2: ")
+        self.deck = Deck()
+        self.p1 = Player(name1)
+        self.p2 = Player(name2)
 
-for card in deck.cards:
-    print(card)
+    def wins(self, winner):
+        w = "{} забариет карты"
+        w = w.format(winner)
+        print(w)
+
+    def draw(self, p1n, p1c, p2n, p2c):
+        d = "{} кладет {}, а {} кладет {}"
+        d = d.format(p1n, p1c, p2n, p2c)
+        print(d)
+
+    def play_game(self):
+        cards = self.deck.cards
+        print("Поехали!")
+        while len(cards) >=2:
+            m = "Нажмите Х для выхода. Нажмите любую другую клавишу \
+            для начала игры"
+            response = input(m)
+            if response == "Х":
+                break            
+            p1c = self.deck.rm_card()
+            p2c = self.deck.rm_card()
+            p1n = self.p1.name
+            p2n = self.p2.name
+            if p1c > p2c:
+                self.p1.wins += 1
+                self.wins(self.p1.name)
+            else:
+                self.p2.wins += 1
+                self.wins(self.p2.name)
+
+        win = self.winner(self.p1, self.p2)
+        print("Игра окончена. {} выиграл!".format(win))
+
+        def winner(self, p1, p2):
+            if p1.wins > p2.wins:
+                return p1.name
+            if p1.wins < p2.wins:
+                return p2.name
+            return "Ничья!"
+
+game = Game()
+game.play_game()
