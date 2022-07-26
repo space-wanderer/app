52c642e9cb6e314e69ce1c77176d72923aff6e76
Vjacheslav Puzanov <v.m.puzanov@yandex.ru>
2022-07-26 22:27:44 +0300

    added player class

--- game.py
+++ game.py
@@ -61,6 +61,12 @@ class Deck:
             return
         return self.cards.pop()
 
+class Player:
+    def __init__(self, name):
+        self.wins = 0
+        self.card = None
+        self.name = name
+
 deck = Deck()
 
 for card in deck.cards:
